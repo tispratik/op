a0419e0bda88a10e51df75dfecb939fda995dab5
Pratik <tispratik@gmail.com>
2010-04-26 01:24:08 -0500

    updates

---
+++ app/controllers/user_sessions_controller.rb
@@ -0,0 +1,33 @@
+class UserSessionsController < ApplicationController
+  before_filter :ssl_required, :except => :destroy
+  
+  def new
+    @user_session = UserSession.new
+  end
+  
+  def create
+    @user_session = UserSession.new(params[:user_session])
+    if @user_session.save
+      flash[:notice] = "Logged in successfully."
+      #Resetting the session to avoid session fixation attacks. This session cannot be reused. Authlogic does not do it.
+      reset_session
+      redirect_to_target_or_default(root_url)
+    else
+      flash[:notice] = "Failed to login! Email and Password do not match."
+      render :action => 'new'
+    end
+  end
+  
+  def destroy
+    if !current_user_session.nil?
+      current_user_session.destroy
+      flash[:notice] = "Logout successful!"
+      User.curr_user = nil
+      @current_user = false
+      #Resetting the session to avoid session fixation attacks. This session cannot be reused. Authlogic does not do it.
+      reset_session
+    end
+    redirect_to root_path
+  end
+  
+end
